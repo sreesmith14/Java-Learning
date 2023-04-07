80bdff0112127aeeb36b2343784a848acda1e6f2
sreesmith14 <130054488+sreesmith14@users.noreply.github.com>
2023-04-07 09:16:24 +0530

    Add files via upload

---
+++ Staticblock.java
@@ -0,0 +1,49 @@
+import static java.lang.Math.*;
+class Staticblock
+{
+	static int num1;
+	int num2;
+	
+	static
+	{	
+		System.out.println("From Static Block2");
+		num1=99;
+	}
+	//First Static Block	
+	static
+	{	
+		System.out.println("From Static Block1");
+		num1=60;
+	}
+
+	
+	
+	//First Non-static Block
+	{
+		System.out.println("From Non-Static Block1");
+		num2 =80;		
+	}
+
+	public static void main(String[] args) {
+		//JavaBlockExample jb = new JavaBlockExample();
+		System.out.println(num1);
+		//System.out.println(jb.num2);
+
+		/*System.out.println(cos(0));		
+		System.out.println(sin(90));
+		System.out.println(sqrt(50));
+		*/
+	}
+
+}
+
+
+/*
+
+static block(static initialiser) -- main() -- non static block, -- methods()
+
+*/
+
+
+
+
