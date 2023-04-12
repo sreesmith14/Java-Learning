53cf0916da06dff3360fc1f4bdb42e738f788070
Sreeram A <130054488+sreesmith14@users.noreply.github.com>
2023-04-12 09:12:04 +0530

    Add files via upload

---
+++ Async Program Task- 11 04 2023/dal/helper/MyDBConnection.java
@@ -0,0 +1,26 @@
+package com.dal.helper;
+
+import java.sql.*;
+import java.lang.*;
+
+public class MyDBConnection {
+	static Connection con;
+
+	public static Connection getDbConnection() {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			DriverManager.getConnection("jdbc:oracle:thin@localhost:1521:xe", "syscom", "123");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+		return con;
+	}
+
+	public static void main(String args[]) {
+
+		System.out.println(getDbConnection());
+	}
+}
---
+++ Async Program Task- 11 04 2023/del/MyExecutor.java
@@ -0,0 +1,5 @@
+package com.del;
+
+public class MyExecutor {
+
+}
